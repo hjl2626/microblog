31413cf2017e2660655c3f719b8157cd53d0fdc0
hjl <799728970@qq.com>
2015-12-19 00:14:32 +0800

    dev-day1

--- app/views.py
+++ app/views.py
@@ -4,19 +4,34 @@
 @version: ??
 @author: hjl
 @license: Apache Licence 
-@contact: endoffight@gmail.com
+@contact: 799728970@gmail.com
 @site: 
 @software: PyCharm
 @file: views.py.py
 @time: 2015/12/18 22:51
 """
 from app import app
+from flask import render_template
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "welcome"
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
 if __name__ == '__main__':
